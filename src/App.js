83c9b3c55c835001410ae0fc9dab615fd421d6c4
Hugo Vila <code.hvila@gmail.com>
2022-02-19 01:51:21 +0100

    Sturdy UIs with enumerated states

--- src/App.js
+++ src/App.js
@@ -1,39 +1,149 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchPeople } from "./api";
 import "./App.css";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
+  const [rstate, setRState] = useState(null);
+
+  const STATES = {
+    idle: "idle",
+    loading: "loading",
+    successfull: "successful",
+    error: "error",
+  };
+
+  function dataFetchMachine() {
+    let mstate = STATES.idle;
+    setRState(STATES.idle);
+
+    return {
+      state() {
+        return mstate;
+      },
+      fetch() {
+        mstate = STATES.loading;
+        setRState(mstate);
+      },
+      resolve() {
+        mstate = STATES.successfull;
+        setRState(mstate);
+      },
+      reject() {
+        mstate = STATES.error;
+        setRState(mstate);
+      },
+    };
+  }
+
+  const [dataFetch, setDataFetch] = useState();
+
+  useEffect(() => {
+    setDataFetch(dataFetchMachine());
+  }, []);
+
+  useEffect(() => {
+    console.log();
+    console.log(dataFetch && dataFetch.state());
+    console.log(rstate);
+  }, []);
+
   function fetchData() {
-    setIsLoading(true);
+    // setIsLoading(true);
+
+    dataFetch.fetch();
+    console.log(dataFetch.state());
+
     fetchPeople()
       .then((r) => r.results)
       .then(
         (res) => {
           setResults(res);
-          setIsLoading(false);
+
+          // setIsLoading(false);
+          // setErrorMessage("");
+
+          dataFetch.resolve();
+          console.log(dataFetch.state(), rstate);
         },
         (message) => {
           setErrorMessage(message);
-          setIsLoading(false);
+
+          // setIsLoading(false);
+
+          dataFetch.reject();
+          console.log(dataFetch.state(), rstate);
         }
       );
   }
 
   return (
     <div className="App">
-      <button onClick={() => fetchData()}>Fetch</button>
-      {isLoading ? <p>Loading</p> : null}
-      {!isLoading && !errorMessage ? (
-        <ul>
-          {results &&
-            results.map((person, index) => <li key={index}>{person.name}</li>)}
-        </ul>
-      ) : null}
-      {!isLoading && errorMessage ? <p>{errorMessage}</p> : null}
+      <div className="controls">
+        <button onClick={() => fetchData()} disabled={rstate === "loading"}>
+          Fetch
+        </button>
+        <div className={rstate + " label"}>{rstate}</div>
+      </div>
+
+      <div className="wrapper enumerated-states">
+        <div>
+          {dataFetch && dataFetch.state() === "idle" && (
+            <p>Clic on Fetch Button to Start Request</p>
+          )}
+
+          {dataFetch && dataFetch.state() === "loading" /** isLoading */ ? (
+            <>
+              <p>Loading</p>
+            </>
+          ) : null}
+          {dataFetch &&
+          dataFetch.state() ===
+            "successful" /** !isLoading && !errorMessage */ ? (
+            <>
+              <ul>
+                {results &&
+                  results.map((person, index) => (
+                    <li key={index}>{person.name}</li>
+                  ))}
+              </ul>
+            </>
+          ) : null}
+          {dataFetch &&
+          dataFetch.state() === "error" /** !isLoading && errorMessage */ ? (
+            <>
+              <p>{errorMessage}</p>
+            </>
+          ) : null}
+        </div>
+
+        <div style={{ maxWidth: "40%" }}>
+          {rstate === "idle" && <p>Clic on Fetch Button to Start Request</p>}
+          {rstate === "loading" /** isLoading */ ? (
+            <>
+              <p>Loading</p>
+            </>
+          ) : null}
+          {rstate === "successful" /** !isLoading && !errorMessage */ ? (
+            <>
+              <ul>
+                {results &&
+                  results.map((person, index) => (
+                    <li key={index}>{person.name}</li>
+                  ))}
+              </ul>
+            </>
+          ) : null}
+          {rstate === "error" /** !isLoading && errorMessage */ ? (
+            <>
+              <p>{errorMessage}</p>
+            </>
+          ) : null}
+        </div>
+      </div>
     </div>
   );
 }
